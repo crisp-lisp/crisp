3aafbb9b055b261ec9705fbb6f067037bd779d39
Saul Johnson <saul.a.johnson@gmail.com>
2015-10-20 20:00:45 +0100

    Add quote function

---
+++ Crisp.Native/QuoteNativeFunction.cs
@@ -0,0 +1,27 @@
+﻿using Crisp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crisp.Native
+{
+    public class QuoteNativeFunction : INativeFunction
+    {
+        public INativeFunctionHost Host { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return "quote";
+            }
+        }
+
+        public SymbolicExpression Apply(SymbolicExpression input)
+        {
+            return input.AsNode().Head; // Argument list is always a node.
+        }
+    }
+}
