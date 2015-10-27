6044adf8febb0a1739028a139909b35d1b809702
Saul Johnson <saul.a.johnson@gmail.com>
2015-10-27 23:54:59 +0000

    Document token stream

--- Crisp/Parsing/TokenStream.cs
+++ Crisp/Parsing/TokenStream.cs
@@ -1,56 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Crisp.Tokenizing;
 
 namespace Crisp.Parsing
 {
+    /// <summary>
+    /// Represents a controlled stream of tokens.
+    /// </summary>
     internal class TokenStream
     {
         private readonly IList<Token> _tokens;
 
+        /// <summary>
+        /// Gets the current position of the stream.
+        /// </summary>
         public int Position { get; private set; }
 
+        /// <summary>
+        /// Returns the next token in the stream, but does not consume it.
+        /// </summary>
+        /// <returns></returns>
         public Token Peek()
         {
             return Position == _tokens.Count ? null : _tokens[Position];
         }
 
+        /// <summary>
+        /// Reads the next token from the stream.
+        /// </summary>
+        /// <returns></returns>
         public Token Read()
         {
             return Position == _tokens.Count ? null : _tokens[Position++];
         }
 
+        /// <summary>
+        /// Reads the next expression (list or atom) from the stream.
+        /// </summary>
+        /// <returns></returns>
         public IList<Token> ReadExpression()
         {
             var expression = new List<Token>();
 
             var buffer = Read();
-            expression.Add(buffer);
+            expression.Add(buffer); // Add buffer to result.
+
+            // If we're going to be reading a list.
             if (buffer.Type == TokenType.OpeningParenthesis)
             {
+                // Create bracket stack.
                 var brackets = new Stack<Token>();
                 brackets.Push(buffer);
                 
+                // Loop while bracket stack is not empty.
                 while (brackets.Any())
                 {
-                    buffer = Read();
-
-                    if (buffer == null)
+                    // Throw if we're at the end of the stream.
+                    if ((buffer = Read()) == null)
                     {
                         var bracket = brackets.Peek();
                         throw new ParsingException($"Mismatched opening parenthesis at line {bracket.Line}" +
                                                    $" column {bracket.Column}.", bracket);
                     }
 
+                    // Push or pop brackets.
                     if (buffer.Type == TokenType.OpeningParenthesis)
                     {
                         brackets.Push(buffer);
                     }
                     else if (buffer.Type == TokenType.ClosingParenthesis)
                     {
+                        // Too many closing brackets.
                         if (!brackets.Any())
                         {
                             throw new ParsingException($"Mismatched closing parenthesis at line {buffer.Line}" +
@@ -59,24 +80,32 @@ namespace Crisp.Parsing
                         brackets.Pop();
                     }
 
-                    expression.Add(buffer);
+                    expression.Add(buffer); // Add buffer to result.
                 }
             }
 
             return expression;
         }
 
+        /// <summary>
+        /// Reads the rest of this token stream.
+        /// </summary>
+        /// <returns></returns>
         public IList<Token> ReadToEnd()
         {
-            var rest = new List<Token>();
-            Token buffer = null;
+            var remaining = new List<Token>();
+            Token buffer;
             while ((buffer = Read()) != null)
             {
-                rest.Add(buffer);
+                remaining.Add(buffer);
             }
-            return rest;
+            return remaining;
         } 
 
+        /// <summary>
+        /// Initializes a new instance of a controlled stream of tokens.
+        /// </summary>
+        /// <param name="tokens">The list of tokens to stream.</param>
         public TokenStream(IList<Token> tokens)
         {
             _tokens = tokens;
