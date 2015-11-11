199fbe491baec3769da6217f87d25faedaa875b3
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-11 18:22:30 +0000

    Add other basic math functions and skeletal letrec

---
+++ Crisp.Native/LetRecNativeFunction.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+using Crisp.Core;
+
+namespace Crisp.Native
+{
+    /// <summary>
+    /// Represents the basic function to bind symbols to expressions, allowing the definition of recursive functions.
+    /// </summary>
+    public class LetRecNativeFunction : IFunction
+    {
+        public IFunctionHost Host { get; set; }
+
+        public string Name => "letrec";
+
+        public SymbolicExpression Apply(SymbolicExpression expression, Context context)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfShorterThanLength(Name, 1); // Must have at least one argument (the body).
+
+            var evaluable = arguments[0];
+
+            var bindings = arguments.Where(a => a != evaluable);
+            var newContext = context;
+            foreach (var binding in bindings)
+            {
+                // Bindings must be formatted as pairs.
+                if (binding.Type != SymbolicExpressionType.Pair)
+                {
+                    throw new RuntimeException(
+                        $"Bindings specified in a {Name} expression must be in the form of pairs.");
+                }
+
+                // The target of each binding must be a symbol.
+                if (binding.AsPair().Head.Type != SymbolicExpressionType.Symbol)
+                {
+                    throw new RuntimeException(
+                        $"Bindings specified in a {Name} expression must bind symbols to expressions.");
+                }
+
+                newContext = newContext.Bind(binding.AsPair().Head.AsSymbol(),
+                    Host.Evaluate(binding.AsPair().Tail, context));
+            }
+
+            return Host.Evaluate(evaluable, newContext);
+        }
+    }
+}
---
+++ Crisp.Native/MulNativeFunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+using Crisp.Core;
+
+namespace Crisp.Native
+{
+    /// <summary>
+    /// Represents the basic multiplication function.
+    /// </summary>
+    public class MulNativeFunction : IFunction
+    {
+        public IFunctionHost Host { get; set; }
+
+        public string Name => "mul";
+
+        public SymbolicExpression Apply(SymbolicExpression expression, Context context)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 2); // Must have two arguments.
+
+            // Attempt to evaluate every argument to a number.
+            var evaluated = arguments.Select(a => Host.Evaluate(a, context)).ToArray();
+            if (evaluated.Any(e => e.Type != SymbolicExpressionType.Numeric))
+            {
+                throw new RuntimeException(
+                    $"The arguments to the function '{Name}' must all evaluate to the numeric type.");
+            }
+
+            return new NumericAtom(evaluated[0].AsNumeric().Value * evaluated[1].AsNumeric().Value);
+        }
+    }
+}
