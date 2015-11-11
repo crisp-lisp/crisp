b5922a4434e332577303afb8a1f87bb5aa78cf1e
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-11 18:17:06 +0000

    Add support for closures

---
+++ Crisp.Core/Closure.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Crisp.Core
+{
+    /// <summary>
+    /// Represents a lambda expression and context bound together.
+    /// </summary>
+    public class Closure : SymbolicExpression, IFunction
+    {
+        /// <summary>
+        /// The lambda that underlies the closure.
+        /// </summary>
+        private readonly Lambda _lambda;
+
+        /// <summary>
+        /// The context captured by the closure.
+        /// </summary>
+        private readonly Context _context;
+
+        public override bool IsAtomic => false;
+
+        public override SymbolicExpressionType Type => SymbolicExpressionType.Function;
+
+        public string Name => null; // Closures are anonymous.
+
+        public IFunctionHost Host { get; set; }
+
+        public SymbolicExpression Apply(SymbolicExpression expression, Context context)
+        {
+            return _lambda.Apply(expression, _context); // Ignore the context we were passed.
+        }
+
+        /// <summary>
+        /// Initializes a new instance of a closure.
+        /// </summary>
+        /// <param name="host">A reference to the function host, usually the executing interpreter.</param>
+        /// <param name="parameters">The list of parameters the closure will take.</param>
+        /// <param name="body">The body of the closure.</param>
+        /// <param name="context">The parent context of the closure.</param>
+        public Closure(IFunctionHost host, IList<SymbolAtom> parameters, SymbolicExpression body, Context context)
+        {
+            _lambda = new Lambda(host, parameters, body);
+            _context = context;
+            Host = host;
+        }
+    }
+}
