a37da58e6ab95fba2b04b46174f70904b94af9a5
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-14 01:17:58 +0000

    Document closure

--- Crisp.Core/Closure.cs
+++ Crisp.Core/Closure.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 using Crisp.Core.Evaluation;
 
 namespace Crisp.Core
 {
+    /// <summary>
+    /// Represents a lambda expression that will always be applied using a specific evaluator.
+    /// </summary>
     public class Closure : Lambda
     {
         private readonly IEvaluator _evaluator;
-
+        
         public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
         {
             return base.Apply(expression, _evaluator); // Ignore passed-in evaluator.
         }
 
+        /// <summary>
+        /// Initializes a new instance of a lambda expression that will always be applied in a specific context.
+        /// </summary>
+        /// <param name="parameters">The list of parameters the lambda will take.</param>
+        /// <param name="body">The body of the lambda.</param>
+        /// <param name="evaluator">The evaluator to use to apply the lambda.</param>
         public Closure(IList<SymbolAtom> parameters, SymbolicExpression body, IEvaluator evaluator) : base(parameters, body)
         {
             _evaluator = evaluator;
