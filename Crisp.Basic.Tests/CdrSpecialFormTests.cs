46e9bd4dcb24618b78e211fd12c1ac1602049944
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-23 22:45:04 +0000

    Add tests for cdr and cons operations

---
+++ Crisp.Basic.Tests/CdrSpecialFormTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace Crisp.Basic.Tests
+{
+    [TestClass]
+    public class CdrSpecialFormTests
+    {
+        private static Fixture _fixture;
+        private static IEvaluator _mockEvaluator;
+
+        [ClassInitialize] 
+        public static void Initialize(TestContext context)
+        {
+            _fixture = new Fixture();
+            _fixture.Register<SymbolicExpression>(() => _fixture.Create<NumericAtom>());
+
+            // Use a dummy evaluator that just returns what it's been given.
+            var mockEvaluator = new Mock<IEvaluator>();
+            mockEvaluator.Setup(m => m.Evaluate(It.IsAny<SymbolicExpression>()))
+                .Returns((SymbolicExpression s) => s);
+            _mockEvaluator = mockEvaluator.Object;
+        }
+
+        [TestMethod]
+        public void CdrSpecialFormShouldReturnListTail()
+        {
+            /**
+             * Description: The cdr special form should return the tail of the pair it is 
+             * given as an argument. This test ensures that this behavior is present.
+             */
+
+            // Special form to test.
+            var function = new CdrSpecialForm();
+            
+            // Compute answer.
+            var head = _fixture.Create<NumericAtom>();
+            var tail = _fixture.Create<NumericAtom>();
+            var args = new List<SymbolicExpression> { new Pair(head, tail) }.ToProperList();
+            var ans = function.Apply(args, _mockEvaluator);
+            
+            // We should have the tail of the pair as a result.
+            Assert.AreEqual(head, ans);
+        }
+        
+        [TestMethod]
+        public void CdrSpecialFormShouldTakeCorrectNumberOfParameters()
+        {
+            /**
+             * Description: This special form only takes a certain number of parameters, this
+             * test ensures that an exception is thrown in case more than that amount is given.
+             */
+
+            var function = new CdrSpecialForm();
+            
+            var arg = _fixture.Create<Pair>();
+            var correct = new List<SymbolicExpression> {arg}.ToProperList();
+            var incorrect = new List<SymbolicExpression> {arg, arg, arg}.ToProperList();
+
+            function.Apply(correct, _mockEvaluator);
+
+            try
+            {
+                function.Apply(incorrect, _mockEvaluator);
+
+                // We should have failed.
+                Assert.Fail("Exception should have been thrown for wrong number of arguments.");
+            }
+            catch (RuntimeException) { }
+        }
+    }
+}
---
+++ Crisp.Basic.Tests/ConsSpecialFormTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace Crisp.Basic.Tests
+{
+    [TestClass]
+    public class ConsSpecialFormTests
+    {
+        private static Fixture _fixture;
+        private static IEvaluator _mockEvaluator;
+
+        [ClassInitialize] 
+        public static void Initialize(TestContext context)
+        {
+            _fixture = new Fixture();
+
+            // Use a dummy evaluator that just returns what it's been given.
+            var mockEvaluator = new Mock<IEvaluator>();
+            mockEvaluator.Setup(m => m.Evaluate(It.IsAny<SymbolicExpression>()))
+                .Returns((SymbolicExpression s) => s);
+            _mockEvaluator = mockEvaluator.Object;
+        }
+
+        [TestMethod]
+        public void ConsSpecialFormShouldReturnCorrectPair()
+        {
+            /**
+             * Description: The cons special form should return a pair with the given head
+             * and tail. This test ensures that this behavior is present.
+             */
+
+            // Special form to test.
+            var function = new ConsSpecialForm();
+            
+            // Compute answer.
+            var head = _fixture.Create<NumericAtom>();
+            var tail = _fixture.Create<NumericAtom>();
+            var args = new List<SymbolicExpression> { head, tail }.ToProperList();
+            var ans = function.Apply(args, _mockEvaluator).AsPair();
+            
+            // We should have the tail of the pair as a result.
+            Assert.AreEqual(head, ans.Head);
+            Assert.AreEqual(tail, ans.Tail);
+        }
+
+        [TestMethod]
+        public void ConsSpecialFormShouldTakeCorrectNumberOfParameters()
+        {
+            /**
+             * Description: This special form only takes a certain number of parameters, this
+             * test ensures that an exception is thrown in case more than that amount is given.
+             */
+
+            var function = new ConsSpecialForm();
+            
+            var arg = _fixture.Create<NumericAtom>();
+            var correct = new List<SymbolicExpression> {arg, arg}.ToProperList();
+            var incorrect = new List<SymbolicExpression> {arg, arg, arg}.ToProperList();
+
+            function.Apply(correct, _mockEvaluator);
+
+            try
+            {
+                function.Apply(incorrect, _mockEvaluator);
+
+                // We should have failed.
+                Assert.Fail("Exception should have been thrown for wrong number of arguments.");
+            }
+            catch (RuntimeException) { }
+        }
+    }
+}
