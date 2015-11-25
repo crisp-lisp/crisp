ada2f3762f4a13441009e36a8eb7bfa7b01babfe
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-25 18:23:39 +0000

    Add boolean atom type

---
+++ Crisp.Core/Types/BooleanAtom.cs
@@ -0,0 +1,21 @@
+﻿namespace Crisp.Core.Types
+{
+    /// <summary>
+    /// Represents an boolean string expression.
+    /// </summary>
+    public sealed class BooleanAtom : Atom<bool>
+    {
+        public override SymbolicExpressionType Type => SymbolicExpressionType.String;
+
+        public override bool Value { get; protected set; }
+
+        /// <summary>
+        /// Initializes a new instance of a boolean atomic expression.
+        /// </summary>
+        /// <param name="value">The value of the expression.</param>
+        public BooleanAtom(bool value)
+        {
+            Value = value;
+        }
+    }
+}
