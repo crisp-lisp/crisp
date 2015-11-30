45f661de4542ae0bd393e48e776b3e243701bd20
Saul Johnson <saul.a.johnson@gmail.com>
2015-11-30 20:23:48 +0000

    Add Crisp.IO library

---
+++ Crisp.IO/FileReadAllTextSpecialForm.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.IO
+{
+    public class FileReadAllTextSpecialForm : SpecialForm
+    {
+        public override string Name => "file-read-all-text";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 1); // Must have one argument.
+
+            // Check file exists.
+            var path = arguments[0].AsString().Value;
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"File {path} could not be found for reading.", path);
+            }
+
+            // Read file.
+            var content = File.ReadAllText(path);
+
+            return new StringAtom(content);
+        }
+    }
+}
