2a105a69e9eafcc2311a796c067655c4de7c9eb8
Saul Johnson <saul.a.johnson@gmail.com>
2015-12-08 19:52:45 +0000

    Document SpecialFormDirectoryPathProvider

--- Crisp/SpecialFormDirectoryPathProvider.cs
+++ Crisp/SpecialFormDirectoryPathProvider.cs
@@ -5,6 +5,10 @@ using Crisp.Core.Preprocessing;
 
 namespace Crisp
 {
+    /// <summary>
+    /// An implementation of a service that returns the fully-qualified directory path of the directory in which 
+    /// compiled special form binaries are stored.
+    /// </summary>
     internal class SpecialFormDirectoryPathProvider : ISpecialFormDirectoryPathProvider
     {
         private readonly IConfigurationProvider _configurationProvider;
@@ -17,6 +21,11 @@ namespace Crisp
                 _configurationProvider.GetConfiguration().SpecialFormDirectory);
         }
 
+        /// <summary>
+        /// Initializes a new instance of a special form directory provider.
+        /// </summary>
+        /// <param name="configurationProvider">The service to use to get the application configuration.</param>
+        /// <param name="interpreterDirectoryPathProvider">The service to use to get the interpreter path.</param>
         public SpecialFormDirectoryPathProvider(IConfigurationProvider configurationProvider,
             IInterpreterDirectoryPathProvider interpreterDirectoryPathProvider)
         {
