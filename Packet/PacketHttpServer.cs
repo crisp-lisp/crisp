72c8dcdc9b8488b80fb2c1903750727b4a680d92
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-19 14:21:10 +0000

    Get data passed through to dynamic webpages

--- Packet/PacketHttpServer.cs
+++ Packet/PacketHttpServer.cs
@@ -22,11 +22,10 @@ namespace Packet
 
         public override void HandleGetRequest(HttpProcessor processor)
         {
-            
             if (processor.HttpUrl.EndsWith(".csp"))
             {
                 var runtime = CrispRuntimeFactory.GetCrispRuntime("public-www/" + processor.HttpUrl);
-                var result = runtime.Run("0");
+                var result = runtime.Run($"\"{processor.HttpUrl}\" \"GET\" nil");
 
                 if (result.Type != SymbolicExpressionType.String)
                 {
@@ -37,40 +36,37 @@ namespace Packet
                     processor.WriteSuccess();
                     processor.OutputStream.Write(result.AsString().Value);
                 }
-
-//            if (processor.HttpUrl.Equals("/Test.png"))
-//            {
-//                Stream fs = File.Open("../../Test.png", FileMode.Open);
-//
-//                processor.WriteSuccess("image/png");
-//                fs.CopyTo(processor.OutputStream.BaseStream);
-//                processor.OutputStream.BaseStream.Flush();
-//            }
-//
-//            Console.WriteLine("request: {0}", processor.HttpUrl);
-//            processor.WriteSuccess();
-//            processor.OutputStream.WriteLine("<html><body><h1>test server</h1>");
-//            processor.OutputStream.WriteLine("Current Time: " + DateTime.Now.ToString());
-//            processor.OutputStream.WriteLine("url : {0}", processor.HttpUrl);
-//
-//            processor.OutputStream.WriteLine("<form method=post action=/form>");
-//            processor.OutputStream.WriteLine("<input type=text name=foo value=foovalue>");
-//            processor.OutputStream.WriteLine("<input type=submit name=bar value=barvalue>");
-//            processor.OutputStream.WriteLine("</form>");
+            }
+            else
+            {
+                // TODO: Serve static files or whatever.
             }
         }
 
         public override void HandlePostRequest(HttpProcessor processor, StreamReader inputStream)
         {
-            Console.WriteLine("POST request: {0}", processor.HttpUrl);
-            string data = inputStream.ReadToEnd();
+            // Get post data.
+            var data = inputStream.ReadToEnd();
 
-            processor.WriteSuccess();
-            processor.OutputStream.WriteLine("<html><body><h1>test server</h1>");
-            processor.OutputStream.WriteLine("<a href=/test>return</a><p>");
-            processor.OutputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+            if (processor.HttpUrl.EndsWith(".csp"))
+            {
+                var runtime = CrispRuntimeFactory.GetCrispRuntime("public-www/" + processor.HttpUrl);
+                var result = runtime.Run($"\"{processor.HttpUrl}\" \"POST\" \"{data}\"");
 
-
+                if (result.Type != SymbolicExpressionType.String)
+                {
+                    // TODO: Error page.
+                }
+                else
+                {
+                    processor.WriteSuccess();
+                    processor.OutputStream.Write(result.AsString().Value);
+                }
+            }
+            else
+            {
+                // TODO: Serve static files or whatever.
+            }
         }
     }
 }
