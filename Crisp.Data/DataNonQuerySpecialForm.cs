f41da62d5756270cfc598816db4af6585fce2537
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-20 22:02:27 +0000

    Add data special form project

---
+++ Crisp.Data/DataNonQuerySpecialForm.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SQLite;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.Data
+{
+    public class DataNonQuerySpecialForm : SpecialForm
+    {
+        public override string Name => "data-non-query";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 2); // Must have two argument.
+
+            // Get arguments.
+            var connectionString = evaluator.Evaluate(arguments[0]).AsString().Value;
+            var query = evaluator.Evaluate(arguments[1]).AsString().Value;
+
+            // Execute SQLite command.
+            int result;
+            using (var connection = new SQLiteConnection($"Data Source={connectionString};Version=3;"))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    result = command.ExecuteNonQuery();
+                }
+            }
+            
+            return new NumericAtom(result);
+        }
+    }
+}
---
+++ Crisp.Data/DataQuerySpecialForm.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.Data
+{
+    public class DataQuerySpecialForm : SpecialForm
+    {
+        public override string Name => "data-query";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 2); // Must have two argument.
+
+            // Get arguments.
+            var connectionString = evaluator.Evaluate(arguments[0]).AsString().Value;
+            var query = evaluator.Evaluate(arguments[1]).AsString().Value;
+
+            // Execute SQLite command.
+            var results = new List<SymbolicExpression>();
+            using (var connection = new SQLiteConnection($"Data Source={connectionString};Version=3;"))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var values = reader.GetValues();
+                            var resultSet = values.AllKeys.Select(k =>
+                                new Pair(new StringAtom(k), new StringAtom(values[k])) as SymbolicExpression).ToList();
+                            results.Add(resultSet.ToProperList());
+                        }
+                    }
+                }
+            }
+            
+            return results.ToProperList();
+        }
+    }
+}
