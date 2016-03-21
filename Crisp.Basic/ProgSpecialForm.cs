387873a201e8b31595c790419c7f372edf9640a8
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-21 21:51:12 +0000

    Add prog special form

---
+++ Crisp.Basic/ProgSpecialForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.Basic
+{
+    /// <summary>
+    /// Represents the basic prog special function.
+    /// </summary>
+    public class ProgSpecialForm : SpecialForm
+    {
+        public override string Name => "prog";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var evaluable = expression.AsPair().Expand();
+            var evaluated = evaluable.Select(evaluator.Evaluate); // Evaluate all arguments once.
+
+            return evaluated.Last(); // Return last item.
+        }
+    }
+}
