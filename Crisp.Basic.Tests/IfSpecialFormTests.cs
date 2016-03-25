5bce7f613646c1e78257abb5bf277a47642f347e
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-25 11:19:58 +0000

    Add tests for concat special form and if special form

---
+++ Crisp.Basic.Tests/IfSpecialFormTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+using Ploeh.AutoFixture;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.Basic.Tests
+{
+    [TestClass]
+    public class IfSpecialFormTests
+    {
+        private static Fixture _fixture;
+        private static IEvaluator _mockEvaluator;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _fixture = new Fixture();
+
+            // Use a dummy evaluator that just returns what it's been given.
+            var mockEvaluator = new Mock<IEvaluator>();
+            mockEvaluator.Setup(m => m.Evaluate(It.IsAny<SymbolicExpression>()))
+                .Returns((SymbolicExpression s) => s);
+            _mockEvaluator = mockEvaluator.Object;
+        }
+
+        [TestMethod]
+        public void IfSpecialFormShouldProduceCorrectResult()
+        {
+            /**
+             * Description: 
+             */
+
+            // Special form to test.
+            var function = new IfSpecialForm();
+
+            // Compute answer.
+            var t = new BooleanAtom(true);
+            var f = new BooleanAtom(false);
+            var args = new List<SymbolicExpression> { t, t, f }.ToProperList();
+            var ans = function.Apply(args, _mockEvaluator);
+
+            // We should have the correct numeric atom as a result.
+            Assert.AreEqual(ans.Type, SymbolicExpressionType.Boolean);
+            Assert.IsTrue(ans.AsBoolean().Value);
+        }
+
+        [TestMethod]
+        public void IfSpecialFormShouldTakeCorrectNumberOfParameters()
+        {
+            /**
+             * Description: This special form only takes a certain number of parameters, this test ensures that an 
+             * exception is thrown in case more than that amount is given.
+             */
+
+            var function = new IfSpecialForm();
+
+            var arg = _fixture.Create<BooleanAtom>();
+            var correct = new List<SymbolicExpression> { arg, arg, arg }.ToProperList();
+            var incorrect = new List<SymbolicExpression> { arg, arg }.ToProperList();
+
+            function.Apply(correct, _mockEvaluator);
+
+            try
+            {
+                function.Apply(incorrect, _mockEvaluator);
+
+                // We should have failed.
+                Assert.Fail("Exception should have been thrown for wrong number of arguments.");
+            }
+            catch (RuntimeException) { }
+        }
+    }
+}
---
+++ Crisp.String.Tests/ConcatSpecialFormTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+using Ploeh.AutoFixture;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.String.Tests
+{
+    [TestClass]
+    public class ConcatSpecialFormTests
+    {
+        private static Fixture _fixture;
+        private static IEvaluator _mockEvaluator;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _fixture = new Fixture();
+
+            // Use a dummy evaluator that just returns what it's been given.
+            var mockEvaluator = new Mock<IEvaluator>();
+            mockEvaluator.Setup(m => m.Evaluate(It.IsAny<SymbolicExpression>()))
+                .Returns((SymbolicExpression s) => s);
+            _mockEvaluator = mockEvaluator.Object;
+        }
+
+        [TestMethod]
+        public void ConcatSpecialFormShouldProduceCorrectResult()
+        {
+            /**
+             * Description: The concat special form should return a a string atom that contains the concatenation of
+             * the two given string atoms as its value. This test ensures that this is the case.
+             */
+
+            // Special form to test.
+            var function = new ConcatSpecialForm();
+
+            // Compute answer.
+            var x = _fixture.Create<StringAtom>();
+            var y = _fixture.Create<StringAtom>();
+            var args = new List<SymbolicExpression> { x, y }.ToProperList();
+            var ans = function.Apply(args, _mockEvaluator);
+
+            // We should have the correct string atom as a result.
+            Assert.AreEqual(ans.Type, SymbolicExpressionType.String);
+            Assert.AreEqual(x.Value + y.Value, ans.AsString().Value);
+        }
+
+        [TestMethod]
+        public void ConcatSpecialFormShouldTakeCorrectNumberOfParameters()
+        {
+            /**
+             * Description: This special form only takes a certain number of parameters, this test ensures that an 
+             * exception is thrown in case more than that amount is given.
+             */
+
+            var function = new ConcatSpecialForm();
+
+            var arg = _fixture.Create<StringAtom>();
+            var correct = new List<SymbolicExpression> { arg, arg }.ToProperList();
+            var incorrect = new List<SymbolicExpression> { arg, arg, arg }.ToProperList();
+
+            function.Apply(correct, _mockEvaluator);
+
+            try
+            {
+                function.Apply(incorrect, _mockEvaluator);
+
+                // We should have failed.
+                Assert.Fail("Exception should have been thrown for wrong number of arguments.");
+            }
+            catch (RuntimeException) { }
+        }
+    }
+}
