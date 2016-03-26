d528013f7650f84723cae60c51635652de468f59
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-26 12:53:18 +0000

    Add string encoding functions

---
+++ Crisp.String/UrlEncodeSpecialForm.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.String
+{
+    /*
+     * Note: This function should be implemented in Crisp as it's not necessary to implement it at this level.
+     */
+
+    /// <summary>
+    /// Represents the basic URL encode function.
+    /// </summary>
+    public class UrlEncodeSpecialForm : SpecialForm // TODO: Implement in Crisp.
+    {
+        public override string Name => "url-encode";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 1); // Must have one argument.
+
+            // Attempt to evaluate every argument to a string.
+            var evaluated = arguments.Select(evaluator.Evaluate).ToArray();
+            if (evaluated.Any(e => e.Type != SymbolicExpressionType.String))
+            {
+                throw new RuntimeException(
+                    $"The arguments to the function '{Name}' must all evaluate to the string type.");
+            }
+
+            return new StringAtom(HttpUtility.UrlEncode(evaluated[0].AsString().Value));
+        }
+    }
+}
