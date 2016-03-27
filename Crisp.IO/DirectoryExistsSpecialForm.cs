80be0b0ed376817c34ca874d33e79091e5fdfe21
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-27 03:14:41 +0100

    Add DirectoryExists special form

---
+++ Crisp.IO/DirectoryExistsSpecialForm.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.IO
+{
+    public class DirectoryExistsSpecialForm : SpecialForm
+    {
+        public override string Name => "directory-exists";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 1); // Must have one argument.
+
+            // Evaluate argument.
+            var evaluated = evaluator.Evaluate(arguments[0]);
+            if (evaluated.Type != SymbolicExpressionType.String)
+            {
+                throw new RuntimeException(
+                    $"The argument to the function '{Name}' must evaluate to the string type.");
+            }
+
+            // Compute filepath.
+            var rawPath = evaluated.AsString().Value;
+            var path = Path.IsPathRooted(rawPath) ? rawPath : Path.Combine(evaluator.SourceFolderPath, rawPath);
+
+            // Return whether or not file exists.
+            return new BooleanAtom(Directory.Exists(path));
+        }
+    }
+}
