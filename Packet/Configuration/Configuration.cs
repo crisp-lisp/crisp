9b01a7db57ca5e4ad3cc4b92a56c9a2518842dc1
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-27 02:43:28 +0100

    Document configuration

--- Packet/Configuration/Configuration.cs
+++ Packet/Configuration/Configuration.cs
@@ -17,14 +17,29 @@ namespace Packet.Configuration
         /// </summary>
         public IEnumerable<string> CrispFileExtensions { get; set; }
 
+        /// <summary>
+        /// Gets or sets a dictionary mapping file extensions to the MIME types they should be served as.
+        /// </summary>
         public IDictionary<string, string> MimeTypeMappings { get; set; }
 
+        /// <summary>
+        /// Gets or sets the path to the configured 500 internal server error page.
+        /// </summary>
         public string InternalServerErrorPage { get; set; }
 
+        /// <summary>
+        /// Gets or sets the path to the configured 404 not found error page.
+        /// </summary>
         public string NotFoundErrorPage { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of filenames that represent directory index files.
+        /// </summary>
         public IEnumerable<string> DirectoryIndices { get; set; } 
 
+        /// <summary>
+        /// Gets or sets a collection of regular expressions that match filepaths that should not be served to clients.
+        /// </summary>
         public IEnumerable<string> DoNotServePatterns { get; set; }
     }
 }
