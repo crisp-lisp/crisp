4efb6834445e71a4c236b05f434b84aea09bf450
Saul Johnson <saul.a.johnson@gmail.com>
2016-03-29 13:52:18 +0100

    Add regex-replace and chr special forms

---
+++ Crisp.String/ChrSpecialForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.String
+{
+    /// <summary>
+    /// An implementation of a function to convert from a numeric ASCII code to its character equivalent.
+    /// </summary>
+    public class ChrSpecialForm : SpecialForm
+    {
+        public override string Name => "chr";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 1); // Must have one argument.
+
+            // Attempt to evaluate every argument to a number.
+            var evaluated = arguments.Select(evaluator.Evaluate).ToArray();
+            if (evaluated.Any(e => e.Type != SymbolicExpressionType.Numeric))
+            {
+                throw new RuntimeException(
+                    $"The arguments to the function '{Name}' must all evaluate to the numeric type.");
+            }
+
+            return new StringAtom(Convert.ToChar(Convert.ToByte(evaluated[0].AsNumeric().Value)).ToString());
+        }
+    }
+}
---
+++ Crisp.String/RegexReplaceSpecialForm.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Crisp.Core;
+using Crisp.Core.Evaluation;
+using Crisp.Core.Types;
+
+namespace Crisp.String
+{
+    /// <summary>
+    /// Represents the basic string replace function.
+    /// </summary>
+    public class RegexReplaceSpecialForm : SpecialForm
+    {
+        public override string Name => "regex-replace";
+
+        public override SymbolicExpression Apply(SymbolicExpression expression, IEvaluator evaluator)
+        {
+            expression.ThrowIfNotList(Name); // Takes a list of arguments.
+
+            var arguments = expression.AsPair().Expand();
+            arguments.ThrowIfWrongLength(Name, 3); // Must have three arguments.
+
+            // Attempt to evaluate every argument to a string.
+            var evaluated = arguments.Select(evaluator.Evaluate).ToArray();
+            if (evaluated.Any(e => e.Type != SymbolicExpressionType.String))
+            {
+                throw new RuntimeException(
+                    $"The arguments to the function '{Name}' must all evaluate to the string type.");
+            }
+
+            var subject = evaluated[0].AsString().Value;
+            var search = evaluated[1].AsString().Value;
+            var replacement = evaluated[2].AsString().Value;
+
+            return new StringAtom(Regex.Replace(subject, search, replacement));
+        }
+    }
+}
