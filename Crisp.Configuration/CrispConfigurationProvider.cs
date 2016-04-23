24711a06acdc4f82cd0c83b3ab2f52a76529de05
Saul Johnson <saul.a.johnson@gmail.com>
2016-04-23 22:04:39 +0100

    Clean up configuration docs

--- Crisp.Configuration/CrispConfigurationProvider.cs
+++ Crisp.Configuration/CrispConfigurationProvider.cs
@@ -5,12 +5,12 @@ using Crisp.Shared;
 namespace Crisp.Configuration
 {
     /// <summary>
-    /// An implementation of an application configuration settings provider.
+    /// An implementation of an application configuration settings provider service.
     /// </summary>
     public class CrispConfigurationProvider : Provider<ICrispConfiguration>, ICrispConfigurationProvider
     {
         /// <summary>
-        /// Initializes a new instance of an application configuration settings provider.
+        /// Initializes a new instance of an application configuration settings provider service.
         /// </summary>
         /// <param name="rawCrispConfigurationProvider">The raw configuration provider service.</param>
         public CrispConfigurationProvider(IRawCrispConfigurationProvider rawCrispConfigurationProvider)
