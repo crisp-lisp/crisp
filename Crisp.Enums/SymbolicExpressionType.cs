3491f33607245ef76c3abf53b1156f04ae8cac55
Saul Johnson <saul.a.johnson@gmail.com>
2016-04-27 12:35:43 +0100

    Document SymbolicExpressionType

--- Crisp.Enums/SymbolicExpressionType.cs
+++ Crisp.Enums/SymbolicExpressionType.cs
@@ -1,17 +1,48 @@
 ﻿namespace Crisp.Enums
 {
     /// <summary>
-    /// An enumeration of different expression data types.
+    /// An enumeration of expression data types.
     /// </summary>
     public enum SymbolicExpressionType
     {
+        /// <summary>
+        /// Specifies that an expression is a pair structure (cons cell).
+        /// </summary>
         Pair,
+
+        /// <summary>
+        /// Specifies that an expression is a symbol.
+        /// </summary>
         Symbol,
+        
+        /// <summary>
+        /// Specifies that an expression is a constant.
+        /// </summary>
         Constant,
+
+        /// <summary>
+        /// Specifies that an expression is numeric.
+        /// </summary>
         Numeric,
+
+        /// <summary>
+        /// Specifies that an expression is a string.
+        /// </summary>
         String,
+
+        /// <summary>
+        /// Specifies that an expression is a function.
+        /// </summary>
         Function,
+
+        /// <summary>
+        /// Specifies that an expression is a boolean value.
+        /// </summary>
         Boolean,
+
+        /// <summary>
+        /// Specifies that an expression is nil.
+        /// </summary>
         Nil
     }
 }
