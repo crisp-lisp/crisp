f17403023da3b77a2633d86bf354ec410748f069
Saul Johnson <saul.a.johnson@gmail.com>
2016-04-28 08:37:21 +0100

    Add head alias to car special form

--- Crisp.Basic/CarSpecialForm.cs
+++ Crisp.Basic/CarSpecialForm.cs
@@ -12,7 +12,7 @@ namespace Crisp.Basic
     /// </summary>
     public class CarSpecialForm : SpecialForm
     {
-        public override IEnumerable<string> Names => new List<string> {"car"};
+        public override IEnumerable<string> Names => new List<string> {"car", "head"};
 
         public override ISymbolicExpression Apply(ISymbolicExpression expression, IEvaluator evaluator)
         {
