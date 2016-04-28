33a4c657e97c40aa01367b292699afec7fad9e39
Saul Johnson <saul.a.johnson@gmail.com>
2016-04-28 07:18:02 +0100

    Document mocking helper classes

--- Crisp.Tokenization.Tests/MockSourceCodeProviderFactory.cs
+++ Crisp.Tokenization.Tests/MockSourceCodeProviderFactory.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
 
-using Moq;
-
-using Crisp.Interfaces.Parsing;
 using Crisp.Interfaces.Shared;
 
 namespace Crisp.Tokenization.Tests
 {
+    /// <summary>
+    /// A helper class for producing mock <see cref="ISourceCodeProvider"/> instances.
+    /// </summary>
     public static class MockSourceCodeProviderFactory
     {
+        /// <summary>
+        /// Gets a mock source code provider that passes back the specified source code.
+        /// </summary>
+        /// <param name="source">The source code to pass back.</param>
+        /// <returns></returns>
         public static ISourceCodeProvider GetMockSourceCodeProvider(string source)
         {
             var mock = new Mock<ISourceCodeProvider>();
--- Crisp.Tokenization.Tests/MockTokenizerConfigurationProviderFactory.cs
+++ Crisp.Tokenization.Tests/MockTokenizerConfigurationProviderFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using Crisp.Enums;
 using Crisp.Interfaces.Tokenization;
@@ -8,8 +7,15 @@ using Moq;
 
 namespace Crisp.Tokenization.Tests
 {
+    /// <summary>
+    /// A helper class for producing mock <see cref="ITokenizerConfigurationProvider"/> instances.
+    /// </summary>
     public static class MockTokenizerConfigurationProviderFactory
     {
+        /// <summary>
+        /// Passes back a tokenizer configuration provider for alphabetic symbols and whitespace only.
+        /// </summary>
+        /// <returns></returns>
         public static ITokenizerConfigurationProvider GetAlphaTokenizerConfigurationProvider()
         {
             var mock = new Mock<ITokenizerConfigurationProvider>();
