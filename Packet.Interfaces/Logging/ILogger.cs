17c3874154286d06b49fc9ef5e9afb22ad30191a
Saul Johnson <saul.a.johnson@gmail.com>
2016-04-29 15:47:57 +0100

    Extract logger

---
+++ Packet.Interfaces/Logging/ILogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Packet.Interfaces.Logging
+{
+    /// <summary>
+    /// Represents a logger.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Writes a line to the log.
+        /// </summary>
+        /// <param name="msg">The line of text to write to the log.</param>
+        void WriteLine(string msg);
+
+        /// <summary>
+        /// Writes a string to the log.
+        /// </summary>
+        /// <param name="msg">The string to write to the log.</param>
+        void Write(string msg);
+
+        /// <summary>
+        /// Logs an exception.
+        /// </summary>
+        /// <param name="ex">The exception to log.</param>
+        void WriteError(Exception ex);
+    }
+}
---
+++ Packet.Logging/ConsoleWindowLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Packet.Interfaces.Logging;
+
+namespace Packet.Logging
+{
+    /// <summary>
+    /// An implementation of a logger that prints messages to the console window.
+    /// </summary>
+    internal class ConsoleWindowLogger : ILogger
+    {
+        public void WriteLine(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Write(string msg)
+        {
+            Console.Write(msg);
+        }
+
+        public void WriteError(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
