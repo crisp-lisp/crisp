6b2b16f7a93408e5dd638cf13662d72636458966
Saul Johnson <saul.a.johnson@gmail.com>
2016-05-09 13:25:23 +0100

    Tidy imports

--- Packet.Server/DynamicPageHttpRequestHandler.cs
+++ Packet.Server/DynamicPageHttpRequestHandler.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
+
 using Crisp.Enums;
 using Crisp.Interfaces.Serialization;
 using Crisp.Interfaces.Types;
