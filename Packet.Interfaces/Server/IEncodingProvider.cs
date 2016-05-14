3a761a3f8cc903bf7bc515ad7d07fea697c64a12
Saul Johnson <saul.a.johnson@gmail.com>
2016-05-14 10:56:43 +0100

    Document EncodingProvider

--- Packet.Interfaces/Server/IEncodingProvider.cs
+++ Packet.Interfaces/Server/IEncodingProvider.cs
@@ -2,8 +2,15 @@
 
 namespace Packet.Interfaces.Server
 {
+    /// <summary>
+    /// Represents a text encoding provider service.
+    /// </summary>
     public interface IEncodingProvider
     {
+        /// <summary>
+        /// Gets the text encoding from this provider.
+        /// </summary>
+        /// <returns></returns>
         Encoding Get();
     }
 }
--- Packet.Server/EncodingProvider.cs
+++ Packet.Server/EncodingProvider.cs
@@ -8,9 +8,12 @@ namespace Packet.Server
 {
     public class EncodingProvider : Provider<Encoding>, IEncodingProvider
     {
+        /// <summary>
+        /// Initializes a new instance of a text encoding provider service.
+        /// </summary>
+        /// <param name="encoding">The text encoding to be passed back by this service.</param>
         public EncodingProvider(Encoding encoding) : base(encoding)
-        {
-            
+        {   
         }
     }
 }
